fdaa3719cbdb812bfccdb4ed4ff897d20a6d4f90
Sultan AlShaikh <146288255+sxltan@users.noreply.github.com>
2023-10-17 16:43:56 +0000

    Week 3 Session 1

--- Week 3/W3 Worksheet 1/G-Exercise-6.c
+++ Week 3/W3 Worksheet 1/G-Exercise-6.c
@@ -15,9 +15,17 @@ int main() {
 
         while (number != -1)
         {
+            if (number >=0 && number <=100) {
+
             printf ("The number %d is between 0-100, however, it is not the termination value -1\n", number);
             printf ("Enter a new number: \n");
             scanf ("%d", &number);
+            }
+
+            else {
+                printf ("The number you entered is not between 0-100\n");
+                break;
+            }
         }
         printf ("The program will be terminated since you entered the value -1\n");
     }
