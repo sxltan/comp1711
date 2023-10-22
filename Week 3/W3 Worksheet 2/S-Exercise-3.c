25f793af4b74626955d174fb06208c063a5e3704
Sultan AlShaikh <146288255+sxltan@users.noreply.github.com>
2023-10-22 16:42:20 +0000

    Week 3 Session 2

--- Week 3/W3 Worksheet 2/S-Exercise-3.c
+++ Week 3/W3 Worksheet 2/S-Exercise-3.c
@@ -1,2 +1,20 @@
 #include <stdio.h>
 
+int str_len (char* sentence) 
+{
+    int length = 0;
+    while (*sentence != '\0')
+    {
+        length++;
+        sentence++;
+    }
+    return length;
+}
+
+int main()
+{
+    char sentence[] = "Hello";
+    int l = str_len(sentence);
+    printf ("The amount of letters in %s is %i\n", sentence, l);
+    return 0;
+}
