a27be96d426db9c98e255b91a2d67b8d27ebab8b
Sultan AlShaikh <146288255+sxltan@users.noreply.github.com>
2023-10-23 15:30:57 +0000

    W3 Worksheet 2

--- Week 3/W3 Worksheet 2/S-Exercise-4.c
+++ Week 3/W3 Worksheet 2/S-Exercise-4.c
@@ -1,2 +1,34 @@
 #include <stdio.h>
 
+int isPrime (int n) {
+    int i;
+    int flag = 1;
+
+    if (n == 0 || n == 1) {
+        flag = 0;
+    }
+
+    for (i = 2; i <= n / 2; i++) {
+        if (n % i == 0) {
+            flag = 0;
+            break;
+        }
+    }
+    return flag;
+}
+
+int main() {
+    int n;
+
+    printf("Enter a integer: ");
+    scanf("%d", &n);
+
+    int flag_result = isPrime(n);
+    if (flag_result == 1) {
+        printf("The number %d is prime\n", n);
+    }
+    else {
+        printf("The number %d is not prime\n", n);
+    }
+    return 0;
+}
